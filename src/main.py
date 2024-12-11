92b5be97a018250ccea3434f55ec8bbd7b891b6e
NONAME0924 <106069698+NONAME0924@users.noreply.github.com>
2024-12-11 15:58:25 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import yaml
-import create_datasets  # 确保用实际模块名替换 your_module
+import create_datasets 
 import setting_and_training
 import warnings
 import os
@@ -8,7 +8,6 @@ import torch
 
 
 def main():
-    # 读取配置文件
     
     print('=========================================Start execution=======================================================')
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -17,20 +16,8 @@ def main():
     
     with open('../config/create_config.yaml', 'r') as file:
         config = yaml.safe_load(file)
-        
-    if config['self_data_name']:
-        if os.path.splitext(config['self_data_name'])[1] == '.xlsx':
-            df = pd.read_excel(config['self_data_name'])
-            csv_name = os.path.splitext(config['self_data_name'])[0]+'.csv'
-            df.to_csv(csv_name)
-            train_loader, dataset = model_use.get_datasets(csv_name)
-        else:
-            train_loader, dataset = model_use.get_datasets(config['self_data_name'])
-        print('==========================================================================================================')
-        print("load datasets are completed.")
-        print('==========================================================================================================')
-        
-    else:
+
+    if config['onlycreate']:
         dataset_creator = create_datasets.Create_datasets(
             excel_name=config['excel_name'],
             csv_name=config['csv_name'],
@@ -39,28 +26,57 @@ def main():
         )
     
         dataset_creator.creat_excel()
-        df = dataset_creator.creat_datasets(total=config['total'])
+        df = dataset_creator.creat_datasets(total=config['onlycreate'])
         dataset_creator.continue_translate_xlsx(df)
         train_loader, dataset = model_use.get_datasets(dataset_creator.csv_name)
         print('==========================================================================================================')
         print("create datasets are completed.")
-        print('==========================================================================================================')
-
+        print('==========================================================================================================')        
+    else:
+        if config['self_data_name']:
+            if os.path.splitext(config['self_data_name'])[1] == '.xlsx':
+                df = pd.read_excel(config['self_data_name'])
+                csv_name = os.path.splitext(config['self_data_name'])[0]+'.csv'
+                df.to_csv(csv_name)
+                train_loader, dataset = model_use.get_datasets(csv_name)
+            else:
+                train_loader, dataset = model_use.get_datasets(config['self_data_name'])
+            print('==========================================================================================================')
+            print("load datasets are completed.")
+            print('==========================================================================================================')
+            
+        else:
     
-    print('==========================================================================================================')
-    print('start training.')
-    print('==========================================================================================================')
-    model = model_use.setting_and_training(train_loader, dataset)
-
-    print('==========================================================================================================')
-    print('training endding.')
-    print('==========================================================================================================')
-    model_use.save(model)
-
-
-    print('==========================================================================================================')
-    print('All task are completed.')
-    print('==========================================================================================================')
+            dataset_creator = create_datasets.Create_datasets(
+                excel_name=config['excel_name'],
+                csv_name=config['csv_name'],
+                datasets_name=config['datasets_name'],
+                lang=config['lang'],
+            )
+        
+            dataset_creator.creat_excel()
+            df = dataset_creator.creat_datasets(total=config['total'])
+            dataset_creator.continue_translate_xlsx(df)
+            train_loader, dataset = model_use.get_datasets(dataset_creator.csv_name)
+            print('==========================================================================================================')
+            print("create datasets are completed.")
+            print('==========================================================================================================')
+    
+        
+        print('==========================================================================================================')
+        print('start training.')
+        print('==========================================================================================================')
+        model = model_use.setting_and_training(train_loader, dataset)
+    
+        print('==========================================================================================================')
+        print('training endding.')
+        print('==========================================================================================================')
+        model_use.save(model)
+    
+    
+        print('==========================================================================================================')
+        print('All task are completed.')
+        print('==========================================================================================================')
 
 if __name__ == "__main__":
     main()
